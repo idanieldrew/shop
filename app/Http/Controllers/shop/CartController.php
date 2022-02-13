ce39b1ea68ff446c6a418782b3396f85ad0ce46a
idanieldrew <danielgia35@gmail.com>
2022-02-13 22:47:48 +0330

    Add cart table

--- app/Http/Controllers/shop/CartController.php
+++ app/Http/Controllers/shop/CartController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers\shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\CartItem;
 use App\Models\Product;
 
 class CartController extends Controller
 {
       public function store(Product $product)
       {
-            dd($product);
+            $cart = Cart::where('user_id', auth()->user()->id)->get();
+            $cartItem = CartItem::where('product_id', $product->id)->get();
+
+            if ($cart) {
+                  if ($cartItem) {
+                        return view('cart.index')->with('اضافه', 'قبلا اضافه کرده اید');
+                  } else {
+                        $cart->cartItems()->create([
+                              'product_id' => $product->id,
+                              'quantity' => $quantity = 1,
+                              'price' => $price = $product->price,
+                              'total' => $quantity * $price,
+                        ]);
+                  }
+            } else {
+                  $newCart = auth()->user()->cart()->create([
+                        'total' => 0,
+                        'status' => false
+                  ]);
+                  $newCart->cartItems()->create([
+                        'product_id' => $product->id,
+                        'quantity' => $quantity = 1,
+                        'price' => $price = $product->price,
+                        'total' => $quantity * $price,
+                  ]);
+            }
       }
 }
---
+++ database/migrations/2022_02_13_154049_create_cart_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('cart_id')->constrained();
+            $table->smallInteger('quantity')->default(1);
+            $table->float('price')->unique();
+            $table->float('total');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
