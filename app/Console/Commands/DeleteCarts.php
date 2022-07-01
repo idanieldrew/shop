ede28f45f88a7a4a2d0fc7b2d8e09b29c19c54da
idanieldrew <danielgia35@gmail.com>
2022-07-01 15:46:37 +0430

    Complete

---
+++ app/Console/Commands/DeleteCarts.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Cart;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class DeleteCarts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'remove:cart';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+//    protected $description = 'Send a Daily email to all users with a word and its meaning';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info("remove");
+
+        Cart::query()->where('updated_at', '>', Carbon::now()->subDays(7))->delete();
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,20 +2,26 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteCarts;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        DeleteCarts::class
+    ];
+
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('remove:cart')
+            ->everyMinute();
     }
 
     /**
@@ -25,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
